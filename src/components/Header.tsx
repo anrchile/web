cb8a6fac83bf15d488456356a9253ee9832b1c5f
Esteban Hidalgo <esteban@7x7.cl>
2024-10-19 10:02:12 -0300

    Inicial

---
+++ src/components/Header.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import * as React from "react";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { cn } from "@/lib/utils";
+
+const components: { title: string; href: string }[] = [
+  {
+    title: "Misión",
+    href: "/mision",
+  },
+  {
+    title: "Visión",
+    href: "/vision",
+  },
+  {
+    title: "Valores",
+    href: "/valores",
+  },
+];
+
+export function Header() {
+  return (
+    <header className="py-4">
+      <NavigationMenuHeader />
+    </header>
+  );
+}
+
+function NavigationMenuHeader() {
+  return (
+    <NavigationMenu className="mx-auto">
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <a href="/">
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Inicio
+            </NavigationMenuLink>
+          </a>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Academia</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+              {components.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                ></ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        {/*  <NavigationMenuItem>
+          <a href="/staff">
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Staff
+            </NavigationMenuLink>
+          </a>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <a href="/gallery">
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Galeria
+            </NavigationMenuLink>
+          </a>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <a href="/contact">
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Contactenos
+            </NavigationMenuLink>
+          </a>
+        </NavigationMenuItem> */}
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
---
+++ src/consts.ts
@@ -0,0 +1,1 @@
+export const DEFAULT_CLASS_MAIN = "container mx-auto px-48 py-8";
